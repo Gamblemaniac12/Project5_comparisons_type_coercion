c82d4984057d4f37484c3ed4ebbc190dcb153ca0
Gamblemaniac12 <calebgambleman@gmail.com>
2023-06-27 11:43:01 -0700

    first commit

---
+++ Js/Project5.js
@@ -0,0 +1,34 @@
+typeof "John"                 // Returns "string"
+typeof 3.14                   // Returns "number"
+typeof NaN                    // Returns "number"
+typeof false                  // Returns "boolean"
+typeof [1,2,3,4]              // Returns "object"
+typeof {name:'John', age:34}  // Returns "object"
+typeof new Date()             // Returns "object"
+typeof function () {}         // Returns "function"
+typeof myCar                  // Returns "undefined" *
+typeof null            
+
+let str = 20;
+//concatenating at the end of integer variable
+str += " Testing '+=' operator: ";  
+str += 42 + ' ';
+str += {} + ' ';
+str += null;
+console.log(str);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+X = 10;
+y = 10;
+document.write(x === y);
+
+document.write(3 == 11);
+
+const a = 3;
+const b = -2;
+console.log(a > 0 && b > 0);
